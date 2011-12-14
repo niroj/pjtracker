a6391fdf86fdc049c5161ead6543694d92cf705d
Niroj Shrestha <niroj77@yahoo.com>
2011-12-14 15:30:58 +0545

    many to many relation for users and projects created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PjtrackerV11::Application.config.secret_token = '8df591a0207a9538d8d543eeae6242c4577d2ecb31eaa4e09d2a7d8202f80f4791a95c3e0e82a5b3a16b7847a614a6e3cd018a57fc230aea9620f822a0086801'
